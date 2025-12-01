173696ebe3c7f4a58e9f70f91b2500917fec5dba
Meowybot <jorgedidier149@gmail.com>
2025-12-01 17:23:34 -0600

    Update state.lua

--- game/state.lua
+++ game/state.lua
@@ -2,10 +2,18 @@ local state = {}
 local fstate = {}
 local astate = {}
 
+state.apath = "assets."
 state.path = "states."
 state.current = "SplashScreen"
 
 function state.emptylove()
+    love.draw = function() end
+    love.quit = function() end
+    love.update = function() end
+    love.keypressed = function() end
+    love.keyreleased = function() end
+    love.mousepressed = function() end
+    love.mousereleased = function() end
 end
 
 function state.switch(towhat)
