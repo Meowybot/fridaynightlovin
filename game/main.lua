7c8829a3699b51cb09d4f90061cc19f024b70dee
Meowybot <jorgedidier149@gmail.com>
2025-12-03 15:43:29 -0600

    Update main.lua

--- game/main.lua
+++ game/main.lua
@@ -3,7 +3,7 @@ function love.load() -- only called once in the whole game
     love.filesystem.mount(love.filesystem.getSourceBaseDirectory(), "", true) --thank you love forums
     --before this line, load external assets folder
     animx = require("animx") -- load animx library
-    state = require("stateswitcher.stateswitcher") -- load state switcher
+    state, fstate, astate = require("state") -- load state switcher
 
 end
 
