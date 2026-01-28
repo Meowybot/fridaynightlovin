30eae3dcc8757dbf7af4dd18d3ca16d57dd445c6
Meowybot <jorgedidier149@gmail.com>
2026-01-28 16:41:31 -0600

    add more stuff

--- assets/data/characters/bf.lua
+++ assets/data/characters/bf.lua
@@ -8,13 +8,20 @@ iconsuffixes = {
   normal = "normal",
   win = "win"
 },
+singduration = 4,
+scale = 1,
+flip = {x = false, y = false},
 healthcolor = {0, 1, 1, 1, false},
 fps = 24,
 offset = {
   x = 0,
   y = 0
 },
+camoffset = {
+  x = 0,
+  y = 0
+},
 anims = {
-  idle = {frames = "1-5"}
+  idle = {frames = "1-5", loop = true}
 }
 }
