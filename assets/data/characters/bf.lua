c5430768afeb7731bfe6a42118a0ef31b63fa811
Meowybot <jorgedidier149@gmail.com>
2026-01-28 16:35:42 -0600

    Update bf.lua

--- assets/data/characters/bf.lua
+++ assets/data/characters/bf.lua
@@ -1,17 +1,20 @@
 return {
 spritesheet = "bf",
+rows = 10,
+columns = 10,
 icon = "bf-",
 iconsuffixes = {
   lose = "loss",
   normal = "normal",
   win = "win"
 },
+healthcolor = {0, 1, 1, 1, false},
 fps = 24,
 offset = {
   x = 0,
   y = 0
 },
 anims = {
-  idle = {}
+  idle = {frames = "1-5"}
 }
 }
