550f02d2a8bf83508decfe0a3f7c94c474f2d374
Meowybot <jorgedidier149@gmail.com>
2026-01-31 12:35:14 -0600

    Update splash.lua

--- assets/states/splash.lua
+++ assets/states/splash.lua
@@ -2,7 +2,19 @@
     TODO
 --]]
 
-astate.splash.start {
-    img = nil
-    gotostate = "mainmenu"
+local splashmenu = {}
+
+splashmenu.anykey = {
+  img = "anykey.png",
+  fps = 24,
+  rows = 1,
+  cols = 9,
+  anims = {
+    idle = {frames = "1-7"},
+    pressed = {frames = "8-9"}
+  },
+  x = 0,
+  y = 630
 }
+
+return splashmenu
