73596b5ab076b06dee847b63bed69345137afbd4
Meowybot <jorgedidier149@gmail.com>
2026-02-01 08:10:52 -0600

    Update mainmenu.lua

--- assets/states/mainmenu.lua
+++ assets/states/mainmenu.lua
@@ -1,8 +1,21 @@
 --main menu info, like buttons
 --astate is info about states in assets, while fstate is the actual state that is loaded in game.love
-astate.mainmenu.storybut = {
-    x = 0,
-    y = 0,
-    img = nil, --for now
-    gotostate = "splash" --for now
+--last comment is not true anymore
+
+local mainmenu = {}
+
+mainmenu.storybut = {
+  img = "anykey.png",
+  fps = 24,
+  rows = 1,
+  cols = 9,
+  anims = {
+    idle = {frames = "1-7"},
+    pressed = {frames = "8-9"} --notfinal
+  },
+  x = 0,
+  y = 0,
+  GoToState = "splash" --for now
 }
+
+return mainmenu
