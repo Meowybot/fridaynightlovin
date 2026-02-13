eb06e9e0b277574ead18a5feebea02cebe167785
Meowybot <jorgedidier149@gmail.com>
2026-02-12 20:01:56 -0600

    start anykey stuff ig

--- game/states/splash.lua
+++ game/states/splash.lua
@@ -1,9 +1,19 @@
 local a = astate["splash"]
 
 local anykey = {
-  img = love.graphics.newImage("assets/images/" .. a.anykey.img)
+  img = love.graphics.newImage("assets/images/" .. a.anykey.img),
+  x = a.anykey.x,
+  y = a.anykey.y,
+  frameW = a.anykey.frameW,
+  frameH = a.anykey.frameH,
+  fps = a.anykey.fps,
+  scale = a.anykey.scale,
 }
 
+anykey.grid = 0
+anykey.anims = 0
+anykey.current = "idle"
+
 function love.draw()
-  love.graphics.draw(anykey.img, 0, 0)
+  love.draw(anykey.img, 0, 0)
 end
