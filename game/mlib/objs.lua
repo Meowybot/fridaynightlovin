b050c1a6b10e9867df985867fc042ffb92872eaf
Meowybot <jorgedidier149@gmail.com>
2026-03-01 10:35:03 -0600

    Update objs.lua

--- game/mlib/objs.lua
+++ game/mlib/objs.lua
@@ -1,1 +1,20 @@
+local objs = {}
 
+local mt_anim = {}
+mt_anim.__index = mt_anim
+local mt_obj = {}
+mt_obj.__index = mt_obj
+local mt_char = setmetatable({}, mt_obj)
+mt_char.__index = mt_char
+local mt_prop = setmetatable({}, mt_obj)
+mt_prop.__index = mt_prop
+
+-- ANIM
+
+-- OBJ
+
+-- CHAR
+
+-- PROP
+
+return objs
