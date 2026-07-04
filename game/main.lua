d3163867476bcad6b2b8adc80ebbbe0cae835830
Meowybot <jorgedidier149@gmail.com>
2026-07-04 09:08:25 -0600

    Update main.lua

--- game/main.lua
+++ game/main.lua
@@ -1,11 +1,9 @@
 function love.load()
-    local success = "Assets already embedded, no mounting required."
-    --local success = love.filesystem.mount(love.filesystem.getSourceBaseDirectory(), "", true)
-    print(success)
-    print("loading libraries")
-    purf = require("mlib.purf")
-    objs = require("mlib.objs")
-    state = require("mlib.state")
-    print("libraries loaded")
-    state.switch("splash")
+  local success = "Assets already embedded, no mounting required."
+  --local success = love.filesystem.mount(love.filesystem.getSourceBaseDirectory(), "", true)
+  print(success)
+  state = require("lib.state")
+  print("loading libraries")
+  print("libraries loaded")
+  state.switch("splash")
 end
